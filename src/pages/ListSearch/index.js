b5c0dc3024ef72eb4f464409102fe1324e8e899b
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-10-21 16:37:10 -0300

    Listando e buscando

--- src/pages/ListSearch/index.js
+++ src/pages/ListSearch/index.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Container, Wrapper } from './styles';
 import Header from '../../components/Header';
 import Item from './Item';
 
+import { search } from '../../service/api';
+
 export default function ListSearch() {
 
   let { username } = useParams()
+  const [items, setItems] = useState([])
+
+  useEffect( () => {
+    function load(){
+      search(username).then(res => {
+        setItems(res.data);
+      })
+    }
+
+    load();
+  }, [])
 
   return (
     <Container>
       <Header search={username} />
       <Wrapper>
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
-        <Item id="0" />
+        { items.map((item, index) => {
+          return (<Item key={index} data={item} />)
+        })}
       </Wrapper>
       
     </Container>
