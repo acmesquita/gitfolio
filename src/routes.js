5975984c0ebf9e0fff4973477ac20956813cacb9
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-05-14 15:02:53 -0300

    Ajustando api e rotas

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 // import { Container } from './styles';
 
-import Main from './pages/Main'
-import ListSearch from './pages/ListSearch';
-import New from './pages/New';
-import Create from './pages/Create';
-import Show from './pages/Show';
+import Main from "./pages/Main";
+import ListSearch from "./pages/ListSearch";
+import New from "./pages/New";
+import Create from "./pages/Create";
+import Show from "./pages/Show";
 
 export default function Routers() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Main} />
-        <Route path="/search/:username" component={ListSearch} />
-        <Route path="/new" component={New} />
-        <Route path="/create/:username" component={Create} />
-        <Route path="/show/:id" component={Show} />
+        <Route exact path="/search/:username" component={ListSearch} />
+        <Route exact path="/new" component={New} />
+        <Route exact path="/create/:username" component={Create} />
+        <Route exact path="/show/:id" component={Show} />
       </Switch>
     </BrowserRouter>
   );
--- src/service/api.js
+++ src/service/api.js
@@ -1,44 +1,44 @@
 // Import axios
-const axios = require('axios')
+const axios = require("axios");
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: "https://gitfolioapi.herokuapp.com/api/v1",
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 const github = axios.create({
   baseURL: "https://api.github.com/users",
   headers: {
-    'Content-Type': 'application/json'
-  }
-})
+    "Content-Type": "application/json",
+  },
+});
 
 // Conexão com a API
-export const search = (term)=>{
+export const search = (term) => {
   return api.get(`/search?q=${term}`);
-}
+};
 
 export const createPortfolio = (body) => {
-  return api.post('/portfolio', body.portfolio)
-}
+  return api.post("/portfolio", body.portfolio);
+};
 
 export const showPortfolio = (id) => {
   return api.get(`/show/${id}`);
-}
+};
 
 // Conexão com o GitHub
 export const getUser = (username) => {
   return github.get(`/${username}`);
-}
+};
 
 export const getUserRepository = (username, public_repos) => {
-  const pages = public_repos > 100 ? parseInt(public_repos/100) + 1 : 1;
-  const list = []
+  const pages = public_repos > 100 ? parseInt(public_repos / 100) + 1 : 1;
+  const list = [];
   for (let page = 1; page <= pages; page++) {
-    let p = github.get(`/${username}/repos?per_page=100&page=${page}`)
-    list.push(p)
+    let p = github.get(`/${username}/repos?per_page=100&page=${page}`);
+    list.push(p);
   }
-  return  Promise.all(list)
-}
+  return Promise.all(list);
+};
